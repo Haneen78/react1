f70be0a37fe061156f81a316b6a057bb07cca428
Haneen78 <4hhaneen4h@gmail.com>
2022-03-01 23:12:16 +0200

    this is addition App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { getMovies } from './Fake/fakeM';
+class Movies extends Component {
+  state = {
+    movies: getMovies(),
+  }
+  delete = (movie) => {
+    const movies = this.state.movies.filter(m => m._id !== movie._id);
+    //console.log(movies);
+    this.setState({ movies: movies });
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  open = () => {
+    Add.style.display = "block"
+
+  }
+  render() {
+    const { length: count } = this.state.movies;
+    if (count === 0) return <p className='text-center display-4'>there's no Movies in database yet</p>
+    return (
+      <main className='container'>
+
+        <p className='p1'>There's {count} Movies in database</p>
+        <div className='div'>
+          <h1 className='text'>
+            <button className='btn' onClick={() => this.open()}><i class="fa fa-plus" id='add'></i></button>
+            Add a new movie
+          </h1>
+        </div>
+        <div className='div1'>
+          <div className='div2'>
+            <i className="fa fa-plus"></i>
+            <form>
+              <input input type="text" placeholder="Title" id="name"></input>
+              <input input type="text" placeholder="Genre" id="name"></input>
+              <input input type="text" placeholder="NumberInStock" id="name"></input>
+              <input input type="text" placeholder="DailyRentalRate" id="name"></input>
+            </form>
+            <button className="save">
+              <i className="fa fa-save"></i> Save
+            </button>
+            <div className="close" onClick={() => this.close()}>
+              <i className="fa fa-window-close"></i>
+            </div>
+          </div>
+        </div>
+        <table className='table table-striped table-hover border mt-5'>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>NumberInStock</th>
+              <th>DailyRentalRate</th>
+              <th>Delet</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie =>
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button className='btn btn-danger' onClick={() => this.delete(movie)}>
+                    <i className='fa fa-trash'></i>
+                  </button>
+                </td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+
+      </main>
+    );
+  }
 }
 
-export default App;
+export default Movies;
